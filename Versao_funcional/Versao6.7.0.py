aff0d8d6d2f3663ffcc17c80fcc5693e89b25ec0
Samuel L. Nogueira <linharescontatotecnico@gmail.com>
2022-12-05 20:00:54 -0300

    Update Versao6.7.0.py

--- Versao_funcional/Versao6.7.0.py
+++ Versao_funcional/Versao6.7.0.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/')
 def raiz():
     return render_template('index.html')
 
